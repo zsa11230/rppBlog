5fe326caa0fe1e5fd10db204a8584ae573950e33
阮晨光 <117815667@qq.com>
2019-12-30 17:37:21 +0800

    修改数据库配置

--- src/database/database.connection.options.ts
+++ src/database/database.connection.options.ts
@@ -2,11 +2,11 @@ import { ConnectionOptions } from "typeorm";
 import { join } from "path";
 const parentDir = join(__dirname, "..");
 const connectionOpts: ConnectionOptions = {
-  type: "postgres",
+  type: "mysql",
   host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
+  port: Number(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_NAME || "typescript-koa",
   entities: [`${parentDir}/entity/**/*.entity.ts`],
   migrations: [`${parentDir}/migration/**/*.migration.ts`],
